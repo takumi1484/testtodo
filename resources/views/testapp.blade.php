31a718f2b469767c5d494b245f37623c888009e2
kuhblume <kasumigaura0524@yahoo.co.jp>
2018-03-28 16:53:37 +0900

    bladeの変更

--- resources/views/testapp.blade.php
+++ resources/views/testapp.blade.php
@@ -1,4 +1,9 @@
-
+<?php
+if (empty($_SERVER['HTTPS'])) {
+    header("Location: https://{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}");
+    exit;
+}
+?>
 <!DOCTYPE html>
 <html lang="{{ config('app.locale') }}"><!--urlのTopの部分を消してadminとすると管理画面にいける-->
 <head>
